11ececb5b716f7b498234f024716f7e84c0bf439
test <test@test.com>
2020-01-09 19:38:55 +0000

    add android install

---
+++ install_android.sh
@@ -0,0 +1,27 @@
+=chat
+telegram
+skype
+k-9 mail
+hiya
+slack
+zoho
+signal
+
+
+=internet
+bom weather
+flipboard
+here we go
+
+=media
+audio book player
+vlc
+
+
+==settings
+authenticator
+ifttt
+
+==utils
+airmore
+ptv
